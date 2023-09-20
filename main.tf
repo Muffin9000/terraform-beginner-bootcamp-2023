ab245b2cd194ee8d87f32f31efeda3d89c35a674
Muffin9000 Muffin9000 <neutralbox420@gmail.com>
2023-09-20 19:29:17 +0000

    #13 - add tf cloud backend and automate tf cloud token geneartion

--- main.tf
+++ main.tf
@@ -1,29 +1,7 @@
-
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   length           = 28
   special          = false
   upper            = false  
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
 }
 
 resource "aws_s3_bucket" "learning-bucket" {
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  description = "(optional) AWS KEY ID"
+}
+
+variable "AWS_DEFAULT_REGION" {
+  type        = string
+  description = "(optional) AWS region"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "(optional) AWS key secret"
+}
